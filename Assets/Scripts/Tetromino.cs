3f85cffdf37e3f1bee1873fa7b49a28fc84773f6
faulknor82 <55365053+faulknor82@users.noreply.github.com>
2023-10-13 11:17:03 -0400

    Updated Board
    Updated LeftBoard.cs and RightBoard.cs.

--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -17,5 +17,10 @@ public struct TetrominoData
 {
     public Tetromino tetromino;
     public Tile tile;
-    public Vector2Int[] cells;
+    public Vector2Int[] cells { get; private set; } // remove get and set if want to create custom shapes
+
+    public void Initialize()
+    {
+        this.cells = Data.Cells[this.tetromino];
+    }
 }
